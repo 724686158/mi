e16ac5ae91f7813efd36ff2295e48b597fbeb9ed
724686158 <solitarius.holic@gmail.com>
2017-05-20 16:37:44 +0800

    version 0.72 修正监控器在多爬虫并行时的bug。

--- mi/mi/middlewares/middleware_monitor.py
+++ mi/mi/middlewares/middleware_monitor.py
@@ -5,6 +5,9 @@ import time
 Time = lambda: time.strftime('%Y-%m-%d %H:%M:%S')
 
 class StatcollectorMiddleware(object):
+    # 在当前容器中，每个爬虫信息上一次的取值
+    history_dic = {}
+
     # 用于初始化,与实际服务无关
     r = redis.Redis(host="127.0.0.1", port="6379", db=0)
     timer = lambda: time.strftime('%Y-%m-%d %H:%M:%S')
@@ -26,11 +29,22 @@ class StatcollectorMiddleware(object):
     def formatStats(self, stats, name):
         time = Time()
         for key in self.stats_keys:
+            sum = 0
             key_value = stats.get(key, None)
-            if not key_value: key_value = 0
-            value = {"value": [time, key_value]}
+            if not key_value:
+                key_value = 0
+            add = 0
+            if self.history_dic.get(key) is None:
+                add = key_value
+                self.history_dic[key] = 0
+            else:
+                add = key_value - self.history_dic.get(key)
+                self.history_dic[key] = key_value
+            now_value = self.r.lindex(key + "_" + name, -1)
+            if now_value is not None:
+                sum = add + eval(now_value)['value'][1]
+            value = {"value": [time, sum]}
             self.r.rpush(key + "_" + name, value)
-        time = Time()
         for key in self.stats_keys:
             sum = 0
             redis_keys = self.r.keys()
--- mi/mi/spiders/spider_taiwan.py
+++ mi/mi/spiders/spider_taiwan.py
@@ -4,19 +4,19 @@ from mi.items import ArticleItem
 from scrapy.spiders import Rule
 from scrapy.linkextractors import LinkExtractor
 import jieba.analyse
-class Spider_pingwest(RedisCrawlSpider):
-    name = 'pingwest'
-    redis_key = 'pingwest:start_urls'
-    allowed_domains = ['pingwest.com']
+class Spider_taiwan(RedisCrawlSpider):
+    name = 'taiwan'
+    redis_key = 'taiwan:start_urls'
+    allowed_domains = ['eng.taiwan.cn']
     rules = [
-        Rule(LinkExtractor(allow=('http://www.pingwest.com/w+'),deny=()),callback='processArticle',follow=True)
+        Rule(LinkExtractor(allow=('eng.taiwan.cn/\w+/\d{6}'),deny=()),callback='processArticle',follow=True)
     ]
 
     def processArticle(self,response):
         try:
             item = ArticleItem()
-            title = response.xpath('''//div[@class='post-head']/h1/text()''').extract()[0]
-            content = ''.join(response.xpath('''//div[@id='sc-container']/p/text()''').extract())
+            title = response.xpath('''//div[@class='en_line2_2left_2']/div[@class="en_text"]/h1/text()''').extract()[0]
+            content = ''.join(response.xpath('''//div[@class='fontsizebox']/div[@class="TRS_Editor"]/p/text()''').extract())
             item['articleTitle'] = title
             item['articleUrl'] = ''.join(response.url)
             item['articleContent'] = content

8eff88ed62ad65fd5df2c1afb8375a0ed264d1bd
omtbreak <2278543193@qq.com>
2017-06-07 22:18:32 +0800

    version0.76 添加当当和国美的爬虫

---
+++ mi/mi/spiders_depot/spider_dangdang.py
@@ -0,0 +1,91 @@
+#  -*- coding: utf-8 -*-
+import scrapy
+import re
+from scrapy_redis.spiders import RedisCrawlSpider
+from scrapy.spiders import Rule
+from scrapy.linkextractors import LinkExtractor
+from mi.items import ECommerceGoodItem
+from mi.items import ECommerceGoodCommentItem
+from mi.items import ECommerceShopCommentItem
+from mi.items import ECommerceShopItem
+
+class Spider_dangdang(RedisCrawlSpider):
+
+    name = 'dangdang.com'
+    redis_key = 'dangdang:start_urls'
+    allowed_domains = ['dangdang.com']
+
+    rules = {
+        Rule(LinkExtractor(allow=('product.dangdang.com/\d+'), deny=()),callback='processGood',follow=True),
+        Rule(LinkExtractor(allow=('shop.dangdang.com/\d+'), deny=()) ,follow=True),
+        Rule(LinkExtractor(allow=('category.dangdang.com/cid\d+'),deny=()),callback='processSearchPage' ,follow=True)
+    }
+
+    # 获取商品信息
+    def processGood(self,response):
+        try:
+            pattern = re.compile(r'\d+')
+            itemId = pattern.findall(response.url)[0]
+        except:
+            print '无效的商品页'
+        else:
+            # 商品信息
+            goodItem = ECommerceGoodItem()
+            goodItem['eCommerceName'] = self.name
+
+            goodItem['goodId'] = itemId
+            title=response.xpath('''//div[@class='name_info']/h1/text()''').extract()[0].strip()
+            goodItem['goodName']=title
+            try:
+                shopId=response.xpath('''//div[@class='service_more_info']/p[@class='title clearfix']//a/@href''').extract()[0]
+                shopId=pattern.findall(shopId)[0]
+                goodItem['shopId']=shopId
+            except:
+                goodItem['shopId']='null'#自营
+            goodItem['goodUrl']=response.url
+
+            goodPrice=response.xpath('''//p[@id='dd-price']/text()''').extract()[0]
+            goodItem['goodPrice']=goodPrice
+            yield goodItem
+            goodCommentItem=ECommerceGoodCommentItem()
+            goodCommentItem['eCommerceName'] = self.name
+            goodCommentItem['goodId']=itemId
+            goodCommentsData=''.join(response.xpath('''//div[@class='pinglun']/a[@id='comm_num_down']/text()''').extract())
+            goodCommentsUrl=response.url+'?point=comment_point'
+            goodCommentItem['goodCommentsUrl']=goodCommentsUrl
+            goodCommentItem['goodCommentsData']=goodCommentsData
+
+            yield goodCommentItem
+            if goodItem['shopId'] != 'null':
+                print goodItem['shopId']
+                shopItem = ECommerceShopItem()
+                shopItem['eCommerceName'] = self.name
+                shopItem['shopId'] = goodItem['shopId']
+                shopName = ''.join(response.xpath('''//div[@class='service_more_info']/p[@class='title clearfix']//a/text()''').extract())
+                shopItem['shopName']=shopName
+                shopUrl=response.xpath('''//div[@class='service_more_info']/p[@class='title clearfix']//a/@href''').extract()[0]
+                shopItem['shopUrl']=shopUrl
+                shopItem['shopLocation']=''
+                shopItem['shopPhoneNumber']=''
+                yield scrapy.Request(url=shopUrl, callback=self.processShop, meta={'shopItem': shopItem})
+
+    def processShop(self,response):
+        shopCommentItem = ECommerceShopCommentItem()
+        shopCommentItem['eCommerceName'] = self.name
+        shopCommentItem['shopId'] = response.meta['shopItem']['shopId']
+        shopCommentItem['shopCommentsUrl'] = ''
+        shopCommentItem['shopCommentsData'] = ''
+        yield response.meta['shopItem']
+        yield shopCommentItem
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ mi/mi/spiders_depot/spider_gome.py
@@ -0,0 +1,102 @@
+#  -*- coding: utf-8 -*-
+from scrapy_redis.spiders import RedisCrawlSpider
+from scrapy.spiders import Rule
+from scrapy.linkextractors import LinkExtractor
+from mi.items import ECommerceGoodItem
+from mi.items import ECommerceGoodCommentItem
+from mi.items import ECommerceShopCommentItem
+from mi.items import ECommerceShopItem
+import re
+import requests
+
+class Spider_gome(RedisCrawlSpider):
+
+    name = 'gome.com.cn'
+    redis_key = 'guomei:start_urls'
+    allowed_domains = ['gome.com.cn']
+
+    rules = {
+        Rule(LinkExtractor(allow=('item.gome.com.cn'),deny=('image')),callback='processGood',follow=True)
+    }
+
+    priceUrlSegment1='https://ss.gome.com.cn/item/v1/d/m/store/'
+    priceUrlSegment2='/'
+    priceUrlSegment3='/N/11010200/110102002/null/flag/item/allStores?callback=allStores&_=1495607488107'
+
+    # 获取商品信息
+    def processGood(self,response):
+        goodItem=ECommerceGoodItem()
+        goodItem['eCommerceName'] = self.name
+        try:
+            title= response.xpath('''//div[@class='hgroup']/h1/text()''').extract()[0]
+        except:
+            print '无效页面'
+            return
+        pos = response.url.find('.html')
+        url2 = response.url[24:pos]
+        lis = url2.split('-')
+        goodItem['goodId'] =lis[1]
+        goodItem['goodName']=title
+        goodItem['goodUrl']=response.url
+        res=requests.get(url=self.priceUrlSegment1+lis[0]+self.priceUrlSegment2+lis[1]+self.priceUrlSegment3)
+        pattern=re.compile(r'salePrice\":\"\d+')
+        pattern2=re.compile(r'\d+')
+        price=pattern2.findall(pattern.findall(res.content)[0])[0]
+        goodItem['goodPrice'] = price
+
+        #商品评论数据
+        pattern4=re.compile(r'goodCommentPercent\":\"\d+')
+        goodCommentPercent = pattern2.findall(pattern4.findall(res.content)[0])[0]
+
+        pattern5 = re.compile(r'star\":\d+')
+        star = pattern2.findall(pattern5.findall(res.content)[0])[0]
+
+        pattern6=re.compile(r'comments\":\d+')
+        comments = pattern2.findall(pattern6.findall(res.content)[0])[0]
+        goodCommentsData={
+            'goodCommentPercent':goodCommentPercent,
+            'star':star,
+            'comments':comments
+        }
+
+        goodCommentItem = ECommerceGoodCommentItem()
+        goodCommentItem['eCommerceName'] = self.name
+        goodCommentItem['goodId'] = lis[1]
+        goodCommentItem['goodCommentsUrl']=''
+        goodCommentItem['goodCommentsData']=str(goodCommentsData)
+        yield goodCommentItem
+
+        isGuomei = ''.join(
+            response.xpath('''//div[@class='right']/div[@class='zy-stores']/span/text()''').extract())
+        if isGuomei is '国美自营':
+            goodItem['shopId'] = 'null'
+            print  isGuomei
+        else:
+            pattern3=re.compile(r'cn/\d+/?')
+            shop = response.xpath('''//div[@class='ly-stores']/h2/a/@href''').extract()[0]
+            shopId=pattern2.findall(pattern3.findall(shop)[0])[0]
+            goodItem['shopId']=shopId
+            yield goodItem
+
+            shopName=response.xpath('''//div[@class='ly-stores']/h2/a/text() | //div[@class='zy-stores']/h2/a/text()''').extract()[0]
+            print shopName
+
+            shopData=''.join(response.xpath('''//div[@class='services-wrapper']/div[@class='services-stars']/span[@class='score']/text()''').extract()[0])
+            print shopData
+
+            shopItem=ECommerceShopItem()
+            shopItem['eCommerceName'] = self.name
+            shopItem['shopId']=shopId
+            shopItem['shopName']=shopName
+            shopItem['shopUrl']=shop
+            shopItem['shopLocation']=''
+            shopItem['shopPhoneNumber']=''
+            yield shopItem
+
+            shopCommentItem=ECommerceShopCommentItem()
+            shopCommentItem['eCommerceName'] = self.name
+            shopCommentItem['shopId']=shopId
+            shopCommentItem['shopCommentsUrl']=''
+            shopCommentItem['shopCommentsData']=shopData
+            yield shopCommentItem
+
